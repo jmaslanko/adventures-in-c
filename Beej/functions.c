0b785c3e4e10ca4d5eea0ae3dc0bbe8ebea632eb
Jeremy Maslanko <maslankoj@gmail.com>
2024-09-07 22:51:03 -0400

    update function

--- Beej/functions.c
+++ Beej/functions.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+void increment(int *p){
+    *p = *p + 1;
+}
+
 int main(void) {
     int i = 10;
-    printf("The value of i is %d\n", i);
-    printf("And its address is %p\n", (void *)&i);
+    int *j = &i;
+
+    printf("i is %d\n", i);
+    printf("i is also %d\n", *j);
+
+    increment(j);
+
+    printf("j is now %p\n", j);
+    printf("i is now %p\n", &i);
 }
