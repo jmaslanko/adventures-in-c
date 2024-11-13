5224c6b39a6a9f43abe5e80a419edd93df5ff772
Jeremy Maslanko <maslankoj@gmail.com>
2024-11-12 20:21:54 -0500

    last of temp

--- KandR/helloworld.c
+++ KandR/helloworld.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+
 int main() {
-    int fahr, celsius;
-    int lower, upper, step;
+    
+    int fahr;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahr = lower;
-    while (fahr <= upper) {
-        celsius = 5*(fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
-        fahr = fahr + step;
+    for (fahr=LOWER; fahr <=UPPER; fahr= fahr + STEP) {
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
     }
 }
