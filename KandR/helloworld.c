df337ababab10536a971519713892c270278935e
Jeremy Maslanko <maslankoj@gmail.com>
2024-11-16 14:42:09 -0500

    playing with chars

--- KandR/helloworld.c
+++ KandR/helloworld.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 
-#define LOWER 0
-#define UPPER 300
-#define STEP 20
-
-
 int main() {
     
-    int fahr;
+    double nc;
 
-    for (fahr=LOWER; fahr <=UPPER; fahr= fahr + STEP) {
-        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-    }
+    for (nc = 0; getchar() != EOF; ++nc)
+        ;
+    printf("%.0f\n", nc);
 }
