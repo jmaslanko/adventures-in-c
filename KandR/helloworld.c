88b2f3deca2197600615282fb7507989cfa4192b
Jeremy Maslanko <maslankoj@gmail.com>
2024-12-01 21:44:00 -0500

    counting words

--- KandR/helloworld.c
+++ KandR/helloworld.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 
-int main() {
-    
-    double nc;
+#define IN 1
+#define OUT 0
 
-    for (nc = 0; getchar() != EOF; ++nc)
-        ;
-    printf("%.0f\n", nc);
+int main() {
+    int c, nl, nw, nc, state;
+
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c=getchar()) != EOF) {
+        ++nc;
+        if (c =='\n') {
+            ++nl;
+        }
+        if (c == ' ' || c == '\n' || c == '\t') {
+            state = OUT;
+        }
+        else if (state == OUT) {
+            state = IN;
+            ++nw;
+        }
+    }
+    printf("%d %d %d\n", nl, nw, nc);
 }
