e866be6b557877d137c42f62000f27c24e3c2ddd
Jeremy Maslanko <maslankoj@gmail.com>
2024-12-11 18:02:11 -0500

    quick mod, finally makes sense

--- KandR/power.c
+++ KandR/power.c
@@ -2,52 +2,8 @@
 
 #define MAXLINE 1000
 
-int max;
-char line[MAXLINE];
-char longest[MAXLINE];
-
-int mygetline(void);
-void copy(void);
-
+int number = 5;
+int mod = 2;
 int main() {
-    int len;
-    extern int max;
-    extern char longest[];
-    
-    max = 0;
-    while ((len = mygetline()) > 0 ) {
-        if (len > max) {
-            max = len;
-            copy();
-        }
-    }
-    if (max > 0 ) {
-        printf("%s", longest);
-    }
-    return 0;
-}
-
-int mygetline(void) {
-    int c, i;
-    extern char line[];
-
-    for (i=0; i<MAXLINE-1 && (c=getchar()) != EOF && c!='\n';++i) {
-        line[i] = c;
-    }
-    if (c=='\n') {
-        line[i] = c;
-        ++i;
-    }
-    line[i] = '\0';
-    return i;
-}
-
-void copy(void) {
-    int i;
-    extern char line[], longest[];
-
-    i = 0;
-    while ((longest[i] = line[i]) != '\0') {
-        ++i;
-    }
+    printf("%d mod %d = %d\n", number, mod, number % mod);
 }
